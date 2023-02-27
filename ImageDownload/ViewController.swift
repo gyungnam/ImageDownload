88ea792f94e103d365e88650bc420960cac72239
gyungnam <7646602@gmail.com>
2023-02-27 22:23:06 +0900

    final

--- ImageDownload/ViewController.swift
+++ ImageDownload/ViewController.swift
@@ -21,26 +21,24 @@ class ViewController: UIViewController {
     }
     @IBAction func loadOne(_ sender: UIButton)
     {
-        imageView_1.download(from:  "https://imgnn.seoul.co.kr/img/upload/2020/10/08/SSI_20201008104519.jpg")
+        imageView_1.download(from: "https://imgnn.seoul.co.kr/img/upload/2020/10/08/SSI_20201008104519.jpg")
     }
     @IBAction func loadTwo(_ sender: UIButton) {
         imageView_2.download(from: "https://i.ytimg.com/vi/sL1yJZFBXdY/maxresdefault.jpg")
     }
     @IBAction func loadThree(_ sender: UIButton) {
-        imageView_3.download(from:             "https://img.insight.co.kr/static/2019/05/05/700/ga6p1u7585cbf0d61xdp.jpg"
-        )
+        imageView_3.download(from: "https://img.insight.co.kr/static/2019/05/05/700/ga6p1u7585cbf0d61xdp.jpg")
     }
-    @IBAction func loadFour(_ sender: UIButton) { imageView_4.download(from: "https://dimg.donga.com/wps/NEWS/IMAGE/2013/09/11/57595179.1.jpg")
+    @IBAction func loadFour(_ sender: UIButton) {
+        imageView_4.download(from: "https://dimg.donga.com/wps/NEWS/IMAGE/2013/09/11/57595179.1.jpg")
     }
     @IBAction func loadAll(_ sender:UIButton)
     {
-        imageView_1.download(from:  "https://imgnn.seoul.co.kr/img/upload/2020/10/08/SSI_20201008104519.jpg")
+        imageView_1.download(from: "https://imgnn.seoul.co.kr/img/upload/2020/10/08/SSI_20201008104519.jpg")
         imageView_2.download(from: "https://i.ytimg.com/vi/sL1yJZFBXdY/maxresdefault.jpg")
-        imageView_3.download(from:             "https://img.insight.co.kr/static/2019/05/05/700/ga6p1u7585cbf0d61xdp.jpg"
-        )
+        imageView_3.download(from: "https://img.insight.co.kr/static/2019/05/05/700/ga6p1u7585cbf0d61xdp.jpg")
         imageView_4.download(from: "https://dimg.donga.com/wps/NEWS/IMAGE/2013/09/11/57595179.1.jpg")
     }
-    
 }
 extension UIImageView{
     func download(from url: URL, contentMode mode: ContentMode = .scaleAspectFit){
